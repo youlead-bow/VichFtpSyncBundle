7f418497dd9cf4ab0ccd79bcf1636efe8790d400
Laurent Francois <l.francois@youlead.fr>
2024-11-13 15:15:47 +0100

    Configuration

--- src/EvenListener/Doctrine/CleanListener.php
+++ src/EvenListener/Doctrine/CleanListener.php
@@ -4,7 +4,23 @@ declare(strict_types=1);
 
 namespace Vich\FtpSyncBundle\EvenListener\Doctrine;
 
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+
 class CleanListener extends BaseListener
 {
+    public function preUpdate(LifecycleEventArgs $event): void
+    {
+        $object = $event->getObject();
 
+        if (!$this->isUploadable($object)) {
+            return;
+        }
+
+        foreach ($this->getUploadableFields($object) as $field) {
+            //$this->handler->clean($object, $field);
+            // TODO A compléter
+        }
+
+        $this->adapter->recomputeChangeSet($event);
+    }
 }
--- src/EvenListener/Doctrine/UploadListener.php
+++ src/EvenListener/Doctrine/UploadListener.php
@@ -4,7 +4,34 @@ declare(strict_types=1);
 
 namespace Vich\FtpSyncBundle\EvenListener\Doctrine;
 
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+
 class UploadListener extends BaseListener
 {
+    public function prePersist(LifecycleEventArgs $event): void
+    {
+        $object = $event->getObject();
+        if (!$this->isUploadable($object)) {
+            return;
+        }
+        foreach ($this->getUploadableFields($object) as $field) {
+            //$this->handler->upload($object, $field);
+            // TODO A compléter
+        }
+    }
 
+    public function preUpdate(LifecycleEventArgs $event): void
+    {
+        $object = $event->getObject();
+        if (!$this->isUploadable($object)) {
+            return;
+        }
+
+        foreach ($this->getUploadableFields($object) as $field) {
+            // TODO A compléter
+            //$this->handler->upload($object, $field);
+        }
+
+        $this->adapter->recomputeChangeSet($event);
+    }
 }
