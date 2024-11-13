8a069fdf67e8ad729a4583eab95e419a143876a9
Flavien Bucheton <f.bucheton@youlead.fr>
2024-11-13 17:13:24 +0100

    Mise en place

--- src/Command/MappingListClassesCommand.php
+++ src/Command/MappingListClassesCommand.php
@@ -6,29 +6,21 @@ declare(strict_types=1);
 namespace Vich\FtpSyncBundle\Command;
 
 
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Vich\FtpSyncBundle\Metadata\MetadataReader;
 
+#[AsCommand(
+    name: "vich:ftp:sync:mapping:list-classes",
+    description: "Recherche des classes ftpsyncable"
+)]
 class MappingListClassesCommand extends Command
 {
     public function __construct(private readonly MetadataReader $metadataReader)
     {
         parent::__construct();
-    }
-
-    public static function getDefaultName(): string
-    {
-        return 'vich:ftp:sync:mapping:list-classes';
-    }
-
-    protected function configure(): void
-    {
-        $this
-            ->setName('vich:ftp:sync:mapping:list-classes')
-            ->setDescription('Recherche des classes ftpsyncable.')
-        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
