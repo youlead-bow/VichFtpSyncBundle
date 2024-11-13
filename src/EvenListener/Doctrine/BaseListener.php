9a89a11dfedff674e49d609a0fb4dbf82956a59d
Laurent Francois <l.francois@youlead.fr>
2024-11-13 15:06:05 +0100

    Configuration

---
+++ src/EvenListener/Doctrine/BaseListener.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vich\FtpSyncBundle\EvenListener\Doctrine;
+
+use Vich\UploaderBundle\EventListener\Doctrine\BaseListener as VichBaseListener;
+
+abstract class BaseListener extends VichBaseListener
+{
+
+}
