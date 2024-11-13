9ee1d9e20098ffe21244c7a799407182b4604649
Laurent Francois <l.francois@youlead.fr>
2024-11-13 14:45:23 +0100

    Configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -6,6 +6,7 @@ declare(strict_types=1);
 namespace Vich\FtpSyncBundle\DependencyInjection;
 
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -17,6 +18,53 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder(): TreeBuilder
     {
+        $treeBuilder = new TreeBuilder('vich_ftp_sync');
+        $rootNode = $treeBuilder->getRootNode();
+        $this->addMappingsSection($rootNode);
+
+        return $treeBuilder;
+    }
+
+
+    private function addMappingsSection(ArrayNodeDefinition $node): void
+    {
+        $node
+            ->children()
+                ->arrayNode('mappings')
+                    ->useAttributeAsKey('id')
+                    ->prototype('array')
+                        ->children()
+                            ->scalarNode('ftp_dsm')->isRequired()->end()
+                            ->scalarNode('destination')->isRequired()->end()
+                            ->arrayNode('namer')
+                                ->addDefaultsIfNotSet()
+                                    ->beforeNormalization()
+                                        ->ifString()
+                                        ->then(static fn ($v) => ['service' => $v, 'options' => []])
+                                    ->end()
+                                    ->children()
+                                        ->scalarNode('service')->defaultNull()->end()
+                                        ->variableNode('options')->defaultNull()->end()
+                                    ->end()
+                                ->end()
+                                ->arrayNode('directory_namer')
+                                    ->addDefaultsIfNotSet()
+                                        ->beforeNormalization()
+                                            ->ifString()
+                                            ->then(static fn ($v) => ['service' => $v, 'options' => []])
+                                        ->end()
+                                    ->children()
+                                        ->scalarNode('service')->defaultNull()->end()
+                                        ->variableNode('options')->defaultNull()->end()
+                                    ->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+
+
 
     }
 }
