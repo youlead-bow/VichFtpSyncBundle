b6e77af3a32c305858b50c399ff0e4aca79d6290
Laurent Francois <l.francois@youlead.fr>
2024-11-14 10:18:54 +0100

    Configuration

--- src/VichFtpSyncBundle.php
+++ src/VichFtpSyncBundle.php
@@ -4,9 +4,17 @@ declare(strict_types=1);
 
 namespace Vich\FtpSyncBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Vich\FtpSyncBundle\DependencyInjection\RegisterMappingDriversPass;
 
 class VichFtpSyncBundle extends AbstractBundle
 {
 
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new RegisterMappingDriversPass());
+    }
 }
