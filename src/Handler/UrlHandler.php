e9e3f03fac8fc65d0dacb9dc0f78fc998bbd5d66
Laurent Francois <l.francois@youlead.fr>
2024-11-14 17:29:23 +0100

    Configuration

---
+++ src/Handler/UrlHandler.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Vich\FtpSyncBundle\Handler;
+
+use League\Flysystem\FilesystemException;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Vich\FtpSyncBundle\Exception\MappingNotFoundException;
+use Vich\FtpSyncBundle\Mapping\PropertyMapping;
+use Vich\FtpSyncBundle\Mapping\PropertyMappingFactory;
+use Vich\FtpSyncBundle\Util\FtpStorage;
+
+readonly class UrlHandler extends AbstractHandler
+{
+    public function resolve(object $obj, string $fieldName): string
+    {
+        $mapping = $this->getMapping($obj, $fieldName);
+
+        return '';
+    }
+}
